56fe4656f511c8af58c7fefb5d560c9b70995ebd
tyler <tpmgbq@umsystem.edu>
2025-04-23 15:09:11 -0500

    Word Guess

--- main.cpp
+++ main.cpp
@@ -2,20 +2,25 @@
 #include "player1.h"
 #include "player2.h"
 #include "guess.h"
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-	player1 p1;
-	player2 p2;
-	guess gs;
-	p1.inputName();
+        player1 p1;
+        player2 p2;
+        guess gs;
+        p1.inputName();
 
-	p1.inputWord();
+        p1.inputWord();
 
-	p2.inputName();
+        system("clear");
 
-	gs.playGame(p1, p2);
+	system("cls");
 
-	return 0;
+        p2.inputName();
+
+        gs.playGame(p1, p2);
+
+        return 0;
 }
