c2f8cd940e9db2aaeac18114df83f9c47125d7a9
Tyler Matheny <tpmatheny@gmail.com>
2025-04-23 15:28:07 -0500

    Word Guess

--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include "player1.h"
 #include "player2.h"
 #include "guess.h"
+#include "winorlin.h"
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-	player1 p1;
-	player2 p2;
-	guess gs;
-	p1.inputName();
+        player1 p1;
+        player2 p2;
+        guess gs;
+        winorlin wl;
 
-	p1.inputWord();
+        p1.inputName();
 
-	p2.inputName();
+        p2.inputName();
 
-	gs.playGame(p1, p2);
+        p1.inputWord();
 
-	return 0;
+        wl.winlin();
+
+        gs.playGame(p1, p2);
+
+        return 0;
 }
